1e3b6d2496647270c6c202d16d09a51bc83b292c
alxnsmith <alx.n.smith@gmail.com>
2023-11-19 10:49:36 +0300

    Fix defining axios base url

--- src/http/axios.js
+++ src/http/axios.js
@@ -1,4 +1,6 @@
 import axios from 'axios';
 
 // set new axios instance
-export default axios.create();
+export default axios.create({
+    baseURL: import.meta.env.VITE_APP_LFM_AXIOS_BASE_URL,
+});
